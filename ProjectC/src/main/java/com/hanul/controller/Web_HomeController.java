0b3e1b95d800b2cbc98e846f07423bed4daf38b1
dog3218 <87794592+dog3218@users.noreply.github.com>
2021-11-28 23:33:08 +0900

    2021-11-28
    합본

---
+++ ProjectC/src/main/java/com/hanul/controller/Web_HomeController.java
@@ -0,0 +1,59 @@
+package com.hanul.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class Web_HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Web_HomeController.class);
+	
+	@RequestMapping("/error")
+	public String error (HttpServletRequest req, Model model) {
+		
+		Throwable error = (Throwable) req.getAttribute("javax.servlet.error.exception"); 
+		StringBuffer msg = new StringBuffer();
+		
+		while( error != null ) {
+			msg.append("<p>").append(error.getMessage() ).append("</p>");
+			error = error.getCause();	// exception 이 발생한 근본적인 원인을 리턴
+		}
+		
+		model.addAttribute("msg", msg.toString());
+		
+		int code = (Integer) req.getAttribute("javax.servlet.error.status_code");
+		return "error/" + (code == 404 ? 404 : "common");
+	}
+	
+	
+	/*
+	 * @RequestMapping(value = "/", method = RequestMethod.GET) public String
+	 * home(Locale locale, Model model, HttpSession session) {
+	 * logger.info("Welcome home! The client locale is {}.", locale);
+	 * 
+	 * Date date = new Date(); DateFormat dateFormat =
+	 * DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+	 * 
+	 * String formattedDate = dateFormat.format(date);
+	 * 
+	 * // session.setAttribute("category", ""); session.removeAttribute("category");
+	 * model.addAttribute("serverTime", formattedDate );
+	 * 
+	 * return "home"; }
+	 */
+	
+	@RequestMapping("/")
+	public String Intro() {
+		
+		
+		return "home";
+	}
+}
---
+++ ProjectC/src/main/webapp/WEB-INF/views/member/member_detail.jsp
@@ -0,0 +1,70 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<style>
+a{text-decoration: none;}
+
+ 
+</style>
+</head>
+<body>
+<!-- 회원정보 보여주기 후 비번 확인 절차로 넘어가기 -->
+<h3>회원 정보</h3>
+		<table class='w-pct40'>
+			<tr>
+				<th class='w-px120'>성명</th>
+				<td >${loginInfo.name }</td>
+			</tr>
+			<tr>
+				<th>이메일</th>
+				<td>${loginInfo.email}</td>
+			</tr>
+			<tr>
+				<th>닉네임</th>
+				<td>${loginInfo.nickname }</td>
+			</tr>
+			
+			<tr>
+				<th>회원유형</th>
+				<td>${loginInfo.type }</td>
+			</tr>
+			
+			<tr>
+				<th>가입날짜</th>
+				<td>${loginInfo.joindate }</td>
+			</tr>
+			<tr>
+				<th>주소</th>
+				<td>${loginInfo.address}</td>
+			</tr>
+		</table>
+	
+	<div class='btnSet'>
+		<a class='btn-fill' onclick = 'go_modify()'>정보 수정</a>
+		<a class='btn-empty' href='<c:url value="/" />'>취소</a>		
+	</div>
+	
+<script type="text/javascript">
+function go_modify(){
+	if(confirm("회원 정보를 수정하시겠습니까?")){
+		location.href="confirm.my";
+		return true;
+	}else{
+		return false;
+	}
+
+}
+
+
+
+
+
+</script>
+
+
+</body>
+</html>
