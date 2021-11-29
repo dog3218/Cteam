ab1c62a3ea662acb4a423df41236b102a355a388
kyeonmook Kim <90747005+gyoengmookKim@users.noreply.github.com>
2021-11-29 23:13:43 +0900

    1129_gm
    컨트롤러 수정

--- ProjectC/src/main/java/com/hanul/controller/App_MookController.java
+++ ProjectC/src/main/java/com/hanul/controller/App_MookController.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +19,12 @@ import com.google.gson.Gson;
 
 import app_board.BoardDAO_GM;
 import app_board.BoardVO;
+import app_community.CommunityVO;
 
 @Controller
 public class App_MookController {
 	@Autowired private BoardDAO_GM dao_gm;
+	@Autowired @Qualifier("cteam") SqlSession sql;
 	
 	@RequestMapping("/all")
 	public void listAll(HttpSession session, Model model, HttpServletResponse response) {
@@ -179,6 +184,44 @@ public class App_MookController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
+		
+	}
+	
+	
+	@RequestMapping("/CommuInsert")
+	public void commuInsert (HttpServletRequest req ,HttpSession session, Model model, HttpServletResponse response) {
+		
+		String email = req.getParameter("email");
+		String title = req.getParameter("title");
+		String content = req.getParameter("content");
+		String subject = req.getParameter("subject");
+		
+		System.out.println(email);
+		CommunityVO vo = new CommunityVO();
+		vo.setWriter(email);
+		vo.setTitle(title);
+		vo.setContent(content);
+		vo.setSubject(subject);
+		
+		
+		int success = sql.insert("app_community.mapper.insert_show_gm", vo);
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(success);
+		PrintWriter out;
+		// 클라이언트에게 응답
+		try {
+			out = response.getWriter();
+			out.println(json);	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		System.out.println(email);
+		System.out.println(title+"이 담겨서 옴");
+		
+		
 		
 	}
 }
